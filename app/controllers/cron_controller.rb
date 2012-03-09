619270ab615e902e1d489a1a8b18dcb3c07aebfe
Robert McLeod <robert.mcleod@itpeople.co.nz>
2012-03-09 16:13:51 +0300

    WIP: tried to get cron working, updated feed_entry

--- app/controllers/cron_controller.rb
+++ app/controllers/cron_controller.rb
@@ -1,11 +1,32 @@
 class CronController < ApplicationController
 
+  before_filter :check_if_there_are_sources
+
+  def check_if_there_are_sources
+    if Source.count < 1
+      return false
+    end
+  end
+
   def index
 
   	@sources = Source.all
-
+	
 	@sources.each do |source|
-		FeedEntry.get_feed( source.url )
+		FeedEntry.update_from_feed( source.url )
+	end
+
+	if FeedEntry.count > 0
+		last_entry_id = FeedEntry.last.object_id
+	else
+		last_entry_id = 0
+	end
+
+	@feed_entries = FeedEntry.find("id > ?", last_entry_id )
+
+	@tags = Tag.all
+	@feed_entries.each do |item|
+		item.check_for_tags( @tags )
 	end
   end
 
--- app/models/feed_entry.rb
+++ app/models/feed_entry.rb
@@ -1,8 +1,30 @@
 class FeedEntry < ActiveRecord::Base
   has_and_belongs_to_many :tags
 
+  def check_for_tags(tags)
+    tags.each do |tag|
+      has_tag = false
+      if title.include? tag.name
+        has_tag = true
+      end
+
+      if summary.include? tag.name
+        has_tag = true
+      end
+
+      if has_tag
+        self.tags << tag
+      end
+    end
+  end
+
   def self.get_feed(feed_url)
     Feedzirra::Feed.fetch_and_parse(feed_url)
+  end
+
+  def self.update_from_feed(feed_url)
+    feed = Feedzirra::Feed.fetch_and_parse(feed_url)
+    add_entries(feed.entries)
   end
   
   private
