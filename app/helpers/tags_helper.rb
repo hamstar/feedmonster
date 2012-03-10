712a22a0fe568be46dc2c314d07acd97680a3b3c
Robert McLeod <hamstar@malakian.(none)>
2012-03-11 06:15:03 +1300

    made linked_tag_string return none if no tags were given

--- app/helpers/tags_helper.rb
+++ app/helpers/tags_helper.rb
@@ -1,6 +1,11 @@
 module TagsHelper
 
   def linked_tag_string(tags)
+
+    if tags.count < 1
+    	return 'none'
+    end
+
     links = Array.new
     tags.each do |t|
       links << link_to( t.name, t )
